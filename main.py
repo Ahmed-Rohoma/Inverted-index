86bd251e116e26e1230f234e33decaf6a8e51515
A7med-Ro7oma <Ra7ouma70@gmail.com>
2022-05-18 21:54:11 +0200

    modified

--- main.py
+++ main.py
@@ -1,43 +1,42 @@
-def low(z):
-    return z.lower()
+def low(char):
+    return char.lower()
 
-def first(z):
-    return z[0]
+def first(char):
+    return char[0]
 
 def squish(x):
     s = set(x)
     return sorted([(i,x.count(i)) for i in s],key = first)
 
-def clean(x):
-    y = ""
-    s = [".",",","-","?"]
-    for i in x:
-        if i in s:
+def clean(line):
+    new_line = ""
+    removed_char = [".",",","-","?"]
+    for char in line:
+        if char in removed_char:
             continue
-        y += i
-    return y
+        new_line += char
+    return new_line
 
-d = {}
-ln = 0
+data = {}
+length = 0
 
-for line in open('test.txt') :
-    print(line)
-    ln += 1
-    line = clean(line)
-    line = line.split()
+for line in open('data.txt') :
+    length += 1
+    line = clean(line)                  # remove useless characters
+    line = line.split()                 
     for word in line :
-        if word not in d :
-            d[word] = []
-            d[word].append(1)
-            d[word].append([ln])
-        else :
-            d[word][0] += 1 
-            d[word][1].append(ln)
-print('list of D ')
-list_D = list(d)
-list_D.sort(key = low)
-for k in list_D :
-    print(k, d[k][0], squish(d[k][1]))
+        if word not in data :              # if exist create new field
+            data[word] = []
+            data[word].append(1)
+            data[word].append([length])
+        else :                          # if not exist add 1 to counter
+            data[word][0] += 1 
+            data[word][1].append(length)       
+
+list_of_Data = list(data)
+list_of_Data.sort(key = low)
+for k in list_of_Data :
+    print(k, data[k][0], squish(data[k][1]))
 
             
 
